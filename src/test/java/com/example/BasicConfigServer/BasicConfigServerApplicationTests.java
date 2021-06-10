7f13528085627d9fa82551173bf124baec57067c
prashanth devalla <you@example.com>
2021-06-10 23:46:51 +0530

    first commit

---
+++ src/test/java/com/example/BasicConfigServer/BasicConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BasicConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
